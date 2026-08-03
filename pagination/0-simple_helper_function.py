1a5ebd42030d3ae7270928d519b3487f42cfe9da
RahimShikhgayibov <rehim1718@gmail.com>
2026-08-03 22:14:05 +0400

    d

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -8,7 +8,3 @@ def index_range(page: int, page_size: int) -> tuple:
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
-<<<<<<< HEAD
-=======
-    
->>>>>>> 8523acfc260256ff1793fc3656d032aaff480da0
